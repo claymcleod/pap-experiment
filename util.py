cac9bf39b0e1d2c76339ed35ff93ff460bea63a3
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-02 12:12:25 -0600

    beginning of activationpool

--- util.py
+++ util.py
@@ -1,8 +1,10 @@
 """Utilities for PAP thesis."""
 
+import numpy as np
 from keras.layers.core import MaskedLayer, Activation, Dropout, Dense, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras import backend as K
+from keras.backend.common import _FLOATX
 from keras.datasets import cifar10, cifar100
 from keras.utils import np_utils
 from keras.models import Graph
@@ -27,16 +29,14 @@ def get_cifar100():
     X_test = X_test.astype('float32') / 255
     return X_train, X_test, Y_train, Y_test
 
+def step(x):
+    return K.switch(x > 0, 1, 0)
 
 class Step(MaskedLayer):
     """Step activation module."""
 
     def __init__(self, **kwargs):
         super(Step, self).__init__(**kwargs)
-
-        def step(x):
-            return K.switch(x > 0, 1, 0)
-
         self.activation = step
 
     def get_output(self, train=False):
@@ -48,6 +48,35 @@ class Step(MaskedLayer):
                   'activation': self.activation.__name__}
         base_config = super(Activation, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+class ActivationPool(MaskedLayer):
+    def __init__(self, activations, **kwargs):
+        self.activations = activations
+        super(ActivationPool, self).__init__(**kwargs)
+
+    def build(self):
+        #input_shape = self.input_shape[1:]
+        coef = 1./len(self.activations)
+        init = coef * np.ones(shape)
+        print(init)
+        #self.alphas = K.variable(np.ones(shape), dtype, name)
+        #self.trainable_weights = [self.alphas]
+
+        #if self.initial_weights is not None:
+        #    self.set_weights(self.initial_weights)
+        #    del self.initial_weights
+
+    #def get_output(self, train):
+    #    X = self.get_input(train)
+    #    pos = K.relu(X)
+    #    neg = self.alphas * (X - abs(X)) * 0.5
+    #    return pos + neg
+
+    #def get_config(self):
+    #    config = {"name": self.__class__.__name__,
+    #              "init": self.init.__name__}
+    #    base_config = super(PReLU, self).get_config()
+    #    return dict(list(base_config.items()) + list(config.items()))
 
 def get_nonpap_model(channels, rows, cols, classes, loss, optimizer):
     model = Graph()
@@ -65,7 +94,6 @@ def get_nonpap_model(channels, rows, cols, classes, loss, optimizer):
     model.add_node(Convolution2D(64, 3, 3, border_mode='same'), input='do1',
                    name='conv2_a')
     model.add_node(Activation('relu'), input='conv2_a', name='relu2_a')
-    model.add_node(Step(), input='conv2_a', name='step2_a')
     model.add_node(Convolution2D(64, 3, 3), input='relu2_a',
                    name='conv2_b')
     model.add_node(Activation('relu'), input='conv2_b', name='relu2_b')
@@ -82,7 +110,7 @@ def get_nonpap_model(channels, rows, cols, classes, loss, optimizer):
     model.add_output(name='output', input='sm')
     model.compile(loss={'output': loss}, optimizer=optimizer)
     return model
-    
+
 def get_pap_model(channels, rows, cols, classes, loss, optimizer):
     model = Graph()
     model.add_input(name='input', input_shape=(channels, rows, cols))
@@ -108,6 +136,41 @@ def get_pap_model(channels, rows, cols, classes, loss, optimizer):
     model.add_node(Step(), input='conv2_b', name='step2_b')
     model.add_node(MaxPooling2D(pool_size=(2, 2)),
                    inputs=['relu2_b', 'step2_b'], name='mp2')
+    model.add_node(Dropout(0.25), input='mp2', name='do2')
+
+    model.add_node(Flatten(), input='do2', name='flatten')
+    model.add_node(Dense(512), input='flatten', name='d1')
+    model.add_node(Activation('relu'), input='d1', name='relu_out')
+    model.add_node(Dropout(0.5), input='relu_out', name='d2')
+    model.add_node(Dense(classes), input='d2', name='d')
+    model.add_node(Activation('softmax'), input='d', name='sm')
+    model.add_output(name='output', input='sm')
+    model.compile(loss={'output': loss}, optimizer=optimizer)
+    return model
+
+
+def get_semipap_model(channels, rows, cols, classes, loss, optimizer):
+    model = Graph()
+    model.add_input(name='input', input_shape=(channels, rows, cols))
+    model.add_node(Convolution2D(32, 3, 3, border_mode='same'), input='input',
+                   name='conv1_a')
+    model.add_node(Activation('relu'), input='conv1_a', name='relu1_a')
+    model.add_node(Step(), input='conv1_a', name='step1_a')
+    model.add_node(Convolution2D(32, 3, 3), input='relu1_a',
+                   name='conv1_b')
+    model.add_node(Activation('relu'), input='conv1_b', name='relu1_b')
+    model.add_node(MaxPooling2D(pool_size=(2, 2)),
+                   input='relu1_b', name='mp1')
+    model.add_node(Dropout(0.25), input='mp1', name='do1')
+
+    model.add_node(Convolution2D(64, 3, 3, border_mode='same'), input='do1',
+                   name='conv2_a')
+    model.add_node(Activation('relu'), input='conv2_a', name='relu2_a')
+    model.add_node(Convolution2D(64, 3, 3), input='relu2_a',
+                   name='conv2_b')
+    model.add_node(Activation('relu'), input='conv2_b', name='relu2_b')
+    model.add_node(MaxPooling2D(pool_size=(2, 2)),
+                   input='relu2_b', name='mp2')
     model.add_node(Dropout(0.25), input='mp2', name='do2')
 
     model.add_node(Flatten(), input='do2', name='flatten')
