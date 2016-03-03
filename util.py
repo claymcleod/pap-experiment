8a3432b34747bf1dd55518c42b20f7458866b0bc
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-02 22:22:48 -0600

    added cifar10

--- util.py
+++ util.py
@@ -184,6 +184,33 @@ def get_mnist_model(activation, lr):
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
     return model
 
+def get_cifar10_model(activation, lr):
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, border_mode='same',
+                        input_shape=(3, 32, 32)))
+    get_activation(model, activation)
+    model.add(Convolution2D(32, 3, 3))
+    get_activation(model, activation)
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    get_activation(model, activation)
+    model.add(Convolution2D(64, 3, 3))
+    get_activation(model, activation)
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    get_activation(model, activation)
+    model.add(Dropout(0.5))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+    sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    return model
+
 # def get_nonpap_model(channels, rows, cols, classes, loss, optimizer):
 #     model = Graph()
 #     model.add_input(name='input', input_shape=(channels, rows, cols))
