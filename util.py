59980cb360a248d6935b49419b6333a9fd8c74a4
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-03 17:28:13 -0600

    latest cifar experiment

--- util.py
+++ util.py
@@ -201,8 +201,8 @@ def get_cifar10_model(activation, lr):
     # model.add(MaxPooling2D(pool_size=(2, 2)))
     # model.add(Dropout(0.25))
     model.add(Flatten(input_shape=(3, 32, 32)))
-    for i in range(0, 32):
-        model.add(Dense(96))
+    for i in range(0, 4):
+        model.add(Dense(9216))
         get_activation(model, activation)
         model.add(Dropout(0.05))
     model.add(Dense(10))
