380f9fafd97da412b0c6708c502d2f52aeda9b5a
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-04 15:03:34 -0600

    added advanced deepcnet

---
+++ deepcnetadv.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import argparse
+
+parser = argparse.ArgumentParser(description='CIFAR10 DeepCNet script for PAP experiment')
+parser.add_argument('activation', type=str, help='activation function')
+parser.add_argument('-l', '--learningrate',default=0.01,
+                    type=float, help='learning rate')
+parser.add_argument('-b', '--batchsize', default=32,
+                    type=int, help='batch size')
+parser.add_argument('-e', '--epochs', default=500,
+                    type=int, help='epochs')
+parser.add_argument('-d','--augmentation', default=False, action='store_true')
+args = parser.parse_args()
+
+data_augmentation = args.augmentation
+activation = args.activation
+learning_rate = args.learningrate
+batch_size = args.batchsize
+epochs = args.epochs
+
+import util
+print()
+print('/==========================\\')
+print("| Dataset: CIFAR10 (DeepCNet adv)")
+print("| Activation: {}".format(activation))
+print("| Learning rate: {}".format(learning_rate))
+print("| Batch size: {}".format(batch_size))
+print("| Epochs: {}".format(epochs))
+print('| Data augmentation: {}'.format(data_augmentation))
+print('\\==========================/')
+print()
+
+X_train, X_test, Y_train, Y_test = util.get_cifar10()
+dcn = util.build_deepcnet(12, 160, activation, final_c1=True)
+util.compile_deepcnet(dcn, 0.01)
+
+dcn.fit(X_train, Y_train,
+          batch_size=batch_size, nb_epoch=epochs,
+          show_accuracy=True,
+          shuffle=True,
+          validation_data=(X_test, Y_test),
+          callbacks=[util.PersistentHistory('./cifar10-deepcnetadv-{}-{}.csv'.format(activation, learning_rate))])
