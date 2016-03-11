c9af48e937a7bef61c64ddca0f5f1f41445d38cf
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-11 13:36:01 -0600

    fixes

--- mcsr/jobs/cifar10/deepcnet_small/relu.job.sh
+++ mcsr/jobs/cifar10/deepcnet_small/relu.job.sh
@@ -1,5 +1,7 @@
 # PBS -l:ngpus=1,nodes=1
 # PBS -M clay.l.mcleod@gmail.com
 
+rm -rf /home/ums/r1842/.theano/compiledir_Linux-3.0--default-x86_64-with-SuSE-11-x86_64-x86_64-2.7.11-64/lock_dir
+
 export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
 python src/deepcnet.py relu small -n -d 0.2
