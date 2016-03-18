33886780e8898b8752aa4bf633a71683fdeb74f1
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-17 21:27:17 -0500

    modifications to deepcnet

--- mcsr/jobs/cifar100/resnet/prelu.job.sh
+++ mcsr/jobs/cifar100/resnet/prelu.job.sh
@@ -5,4 +5,4 @@
 rm -rf /home/ums/r1842/.theano/compiledir_Linux-3.0--default-x86_64-with-SuSE-11-x86_64-x86_64-2.7.11-64/lock_dir
 
 export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
-python src/resnet.py prelu reg -l ${lr}
+python src/resnet.py prelu -l ${lr}
