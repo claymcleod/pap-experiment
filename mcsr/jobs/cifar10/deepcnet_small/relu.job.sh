a769b3aaaa43689ac638495ccbe4b7c393d59f90
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-03-18 13:08:19 -0500

    latest

--- mcsr/jobs/cifar10/deepcnet_small/relu.job.sh
+++ mcsr/jobs/cifar10/deepcnet_small/relu.job.sh
@@ -5,4 +5,4 @@
 rm -rf /home/ums/r1842/.theano/compiledir_Linux-3.0--default-x86_64-with-SuSE-11-x86_64-x86_64-2.7.11-64/lock_dir
 
 export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
-python src/deepcnet.py relu small -l ${lr} -n -d 0.2
+python src/deepcnet.py relu small -l ${lr} -d 0.2
